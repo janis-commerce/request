d763fb2381b26e07129d6b434ffa45bbd31b8aed
Diego Brusa <diego.brusa@fizzmod.com>
2021-01-18 19:03:50 -0300

    Package init

---
+++ tests/request-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('assert');
+
+const sinon = require('sinon');
+
+const {
+	Request,
+	RequestError
+} = require('../lib');
+
+describe('Request', () => {
+
+	// your tests here...
+
+});
